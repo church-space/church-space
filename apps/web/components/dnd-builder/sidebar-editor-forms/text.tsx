1a2d526848742796dfb485f8f2e99be1208399fe
Thomas Harmond <teharmond@gmail.com>
2025-02-23 19:14:13 -0600

    forms done

--- apps/web/components/dnd-builder/sidebar-editor-forms/text.tsx
+++ apps/web/components/dnd-builder/sidebar-editor-forms/text.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import { Label } from "@trivo/ui/label";
 
 export default function TextForm() {
-  return <div>TextForm</div>;
+  return (
+    <div className="flex flex-col gap-10 px-2">
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-between items-center">
+          <Label className="font-bold text-md">Details</Label>
+        </div>
+        <div className="text-sm text-muted-foreground">
+          Add your content to the text block, and make chagnes to the formating
+          using the toolbar above.
+        </div>
+      </div>
+    </div>
+  );
 }
