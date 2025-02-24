6320b14231893e1000f9a716137e417643402cdf
Thomas Harmond <teharmond@gmail.com>
2025-02-24 17:28:34 -0600

    import

--- apps/web/app/(app)/(without-sidebar)/emails/[emailId]/editor/page.tsx
+++ apps/web/app/(app)/(without-sidebar)/emails/[emailId]/editor/page.tsx
@@ -1,5 +1,4 @@
 import DndProvider from "@/components/dnd-builder/dnd-provider";
-import { createClient } from "@trivo/supabase/server";
 
 type Params = Promise<{ emailId: string }>;
 
@@ -7,6 +6,5 @@ export default async function Page(props: { params: Params }) {
   const params = await props.params;
   const emailId = parseInt(params.emailId, 10);
 
-  // We don't need to fetch data here since DndProvider will fetch it
   return <DndProvider emailId={emailId} />;
 }
