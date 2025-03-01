e862ad691e19ae9a3d685e1aa6f55c327cada0c5
Thomas Harmond <teharmond@gmail.com>
2025-03-01 10:29:48 -0600

    wip

--- apps/web/components/dnd-builder/email-preview.tsx
+++ apps/web/components/dnd-builder/email-preview.tsx
@@ -17,7 +17,6 @@ export default function EmailPreview() {
       <TabsList>
         <TabsTrigger value="web">Web</TabsTrigger>
         <TabsTrigger value="mobile">Mobile</TabsTrigger>
-        <TabsTrigger value="plain-text">Plain Text</TabsTrigger>
       </TabsList>
       <TabsContent value="web">
         {" "}
@@ -28,24 +27,6 @@ export default function EmailPreview() {
       <TabsContent value="mobile" className="overflow-auto h-full">
         <div className="flex flex-col gap-4 mx-auto max-w-sm w-full h-full items-start">
           {/* EMAIL HERE */}
-        </div>
-      </TabsContent>
-      <TabsContent value="plain-text">
-        <div className=" mx-auto w-full max-w-2xl">
-          Ad nulla laboris cillum et proident aliqua irure commodo exercitation
-          incididunt. Aute sit pariatur duis velit consequat voluptate. Est
-          voluptate deserunt et sit voluptate fugiat reprehenderit ea anim.
-          Officia anim laborum incididunt culpa reprehenderit do. Id nisi culpa
-          labore do officia labore sint commodo velit amet officia. Qui do
-          proident pariatur tempor eu anim veniam cupidatat fugiat aute. Minim
-          qui exercitation consectetur veniam culpa amet est Lorem deserunt quis
-          quis fugiat. Sit consequat minim ut adipisicing dolore consequat
-          culpa. Dolor est consequat laborum enim quis pariatur exercitation
-          veniam commodo. Veniam sit do amet occaecat fugiat dolor voluptate
-          sunt incididunt pariatur officia mollit. Culpa sit et ullamco sit
-          Lorem qui sint anim incididunt incididunt adipisicing. Sit proident
-          elit veniam ipsum amet aute duis minim pariatur proident est aute
-          minim nostrud.
         </div>
       </TabsContent>
     </Tabs>
