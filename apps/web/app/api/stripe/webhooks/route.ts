3d3a1e05fc00e2936b84ab9fb951f62d349052f3
Thomas Harmond <teharmond@gmail.com>
2025-03-18 14:39:05 -0500

    wip

--- apps/web/app/api/stripe/webhooks/route.ts
+++ apps/web/app/api/stripe/webhooks/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  console.log("Stripe webhook received");
+  console.log("Stripe webhook received", request.body);
   return NextResponse.json({ message: "Stripe webhook received" });
 }
