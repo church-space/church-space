4507df4461eb10abd74240e73306b632ba053383
Thomas Harmond <teharmond@gmail.com>
2025-03-25 15:00:51 -0500

    wip

--- packages/supabase/src/queries/all/get-people-with-emails.ts
+++ packages/supabase/src/queries/all/get-people-with-emails.ts
@@ -32,7 +32,10 @@ export async function getPeopleCount(
   if (params?.searchTerm) {
     const searchTerm = `%${params.searchTerm}%`;
     query = query.or(
-      `first_name.ilike.${searchTerm},last_name.ilike.${searchTerm}`
+      `first_name.ilike.${searchTerm},` +
+        `last_name.ilike.${searchTerm},` +
+        `concat(first_name, ' ', last_name).ilike.${searchTerm},` +
+        `people_emails.email.ilike.${searchTerm}`
     );
   }
 
@@ -98,7 +101,10 @@ export async function getPeopleWithEmailsAndSubscriptionStatus(
   if (params?.searchTerm) {
     const searchTerm = `%${params.searchTerm}%`;
     query = query.or(
-      `first_name.ilike.${searchTerm},last_name.ilike.${searchTerm}`
+      `first_name.ilike.${searchTerm},` +
+        `last_name.ilike.${searchTerm},` +
+        `concat(first_name, ' ', last_name).ilike.${searchTerm},` +
+        `people_emails.email.ilike.${searchTerm}`
     );
   }
 
