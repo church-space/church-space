b27e288641dcc7b219a092e118e1874d7857fa38
Thomas Harmond <teharmond@gmail.com>
2025-03-25 07:49:26 -0500

    wip

---
+++ apps/web/app/(app)/(with-sidebar)/emails/categories/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="m-8 rounded-lg border bg-muted p-6 font-mono text-sm text-muted-foreground">
+      Table with sheet. Sheet should have if it's public and the description.
+      Part of onboarding after PCO should be selecting which list categories are
+      public/let the user create list categories from Church Space that they
+      want to use as public. I think it's a little complciated, but it keeps the
+      management of lists in PCO so there's not extra steps for the user.
+    </div>
+  );
+}
