550c808e081c298f945fba3c638afcf27da9236e
Thomas Harmond <teharmond@gmail.com>
2025-04-05 17:14:32 -0500

    wip

--- apps/web/components/sidebar/app-sidebar.tsx
+++ apps/web/components/sidebar/app-sidebar.tsx
@@ -2,14 +2,34 @@
 
 import { Link, List, Mail, QrCode, Settings2, User } from "lucide-react";
 import * as React from "react";
+import { useState } from "react";
 
-import { ChurchSpaceBlack } from "@church-space/ui/icons";
+import { Button } from "@church-space/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@church-space/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@church-space/ui/dropdown-menu";
+import {
+  ChurchSpaceBlack,
+  NewEmail as NewEmailIcon,
+  NewQrCode,
+} from "@church-space/ui/icons";
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
 } from "@church-space/ui/sidebar";
+import NewEmail from "../forms/new-email";
 import { NavMain } from "./nav-main";
 import { NavUser } from "./nav-user";
 import SidebarHelpMenu from "./sidebar-help-menu";
@@ -89,23 +109,63 @@ const data = {
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const [newEmailDialogOpen, setNewEmailDialogOpen] = useState(false);
+
   return (
-    <Sidebar variant="inset" {...props}>
-      <SidebarHeader>
-        <div className="flex w-full items-center justify-between gap-2">
-          <div className="flex items-center gap-0.5">
-            <ChurchSpaceBlack height={"22"} width={"22"} />
-            <span className="text-sm font-bold">ChurchSpace</span>
+    <>
+      <Sidebar variant="inset" {...props}>
+        <SidebarHeader>
+          <div className="flex w-full items-center justify-between gap-2">
+            <div className="flex items-center gap-0.5">
+              <ChurchSpaceBlack height={"22"} width={"22"} />
+              <span className="text-sm font-bold">ChurchSpace</span>
+            </div>
+            <NavUser />
           </div>
-          <NavUser />
-        </div>
-      </SidebarHeader>
-      <SidebarContent>
-        <NavMain items={data.navMain} />
-      </SidebarContent>
-      <SidebarFooter className="flex justify-center text-muted-foreground">
-        <SidebarHelpMenu />
-      </SidebarFooter>
-    </Sidebar>
+        </SidebarHeader>
+        <SidebarContent>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button className="mx-2 mt-2 h-8 shadow-md">Create New</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-[--radix-dropdown-menu-trigger-width]">
+              <DropdownMenuItem
+                className="w-full"
+                onClick={() => setNewEmailDialogOpen(true)}
+              >
+                <NewEmailIcon />
+                New Email
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="w-full"
+                onClick={() => setNewEmailDialogOpen(true)}
+              >
+                <NewQrCode />
+                New QR Link
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <NavMain items={data.navMain} />
+        </SidebarContent>
+        <SidebarFooter className="flex justify-center text-muted-foreground">
+          <SidebarHelpMenu />
+        </SidebarFooter>
+      </Sidebar>
+      <Dialog open={newEmailDialogOpen} onOpenChange={setNewEmailDialogOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle className="flex items-center gap-2">
+              <NewEmailIcon />
+              Create New Email
+            </DialogTitle>
+            <DialogDescription className="text-pretty">
+              What&apos;s the subject of your email? You can always change it
+              later.
+            </DialogDescription>
+          </DialogHeader>
+          <NewEmail organizationId={"43d2f23f-82a8-4eca-b6de-174ca0f9a1a0"} />
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
