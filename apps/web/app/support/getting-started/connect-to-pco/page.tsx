a12cb56ee192acc1b5978a64e857a7b0dfd60bdc
Thomas Harmond <teharmond@gmail.com>
2025-04-07 13:29:15 -0500

    wip

---
+++ apps/web/app/support/getting-started/connect-to-pco/page.tsx
@@ -0,0 +1,37 @@
+import ConnectToPcoMdx from "@/markdown/support/getting-started/connect-to-pco.mdx";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@church-space/ui/breadcrumb";
+
+export default function ConnectToPco() {
+  return (
+    <div className="mx-auto flex w-full max-w-4xl flex-col px-4 pb-20">
+      <header className="mb-10 mt-4 w-full">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/support">Support</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/support/getting-started">
+                Getting Started
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>Connect to PCO</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </header>
+
+      <ConnectToPcoMdx />
+    </div>
+  );
+}
