ea683e1888cc464d42d2a2824a863b1f7b71a203
Thomas Harmond <teharmond@gmail.com>
2025-04-14 16:14:12 -0500

    wip

--- apps/web/app/(marketing)/about/page.tsx
+++ apps/web/app/(marketing)/about/page.tsx
@@ -3,6 +3,40 @@ import React from "react";
 
 // TODO: about page
 
-export default function page() {
-  return <div>page</div>;
+export default function AboutPage() {
+  return (
+    <div className="mx-auto my-20 w-full max-w-4xl space-y-4 px-6">
+      <h1 className="w-full pb-4 text-left text-5xl font-bold leading-tight md:text-center">
+        Why we&apos;re building <br />
+        Church Space
+      </h1>
+      <p className="text-lg">
+        Church Space exists to free ministries from app overload. That journey
+        begins with communication tools.
+      </p>
+      <p className="text-lg">
+        In ministry, I&apos;ve consistently felt that I&apos;ve been stuck when
+        it comes to software. Our church has either overpaid for enterprise
+        software that does far more than we need or we&apos;ve used software
+        made for churches and found it lacking when it comes to features and
+        craft.
+      </p>
+      <p className="text-lg">
+        So we set out with a simple goal:{" "}
+        <b>
+          create well-crafted, affordable software that help local churches
+          operate effectively, communicate clearly, and care intentaiolly.
+        </b>
+      </p>
+      <p className="text-lg">
+        We&apos;re excited to be on this journey, and we can&apos;t wait to
+        serve you.
+      </p>
+      <p className="pt-8 text-lg">
+        <b>Thomas Harmond</b>
+        <br />
+        Founder
+      </p>
+    </div>
+  );
 }
