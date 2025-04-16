c372e08ced7db199e212817183b6198101fd0380
Thomas Harmond <teharmond@gmail.com>
2025-04-16 08:25:52 -0500

    wip

---
+++ apps/web/app/(app)/(with-sidebar)/people/import/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      Import Page. upload CSV, make sure that the column is marked as Email
+      Address, and then say if these contacts should be marked as unsubscribed
+      or cleaned
+    </div>
+  );
+}
