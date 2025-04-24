b5633e2e569c6a66884a7f439d36a2b556c4889e
Thomas Harmond <teharmond@gmail.com>
2025-04-23 23:06:46 -0500

    wip

--- apps/web/app/hello/page.tsx
+++ apps/web/app/hello/page.tsx
@@ -3,13 +3,7 @@ import { createClient } from "@church-space/supabase/server";
 import { redirect } from "next/navigation";
 import ClientPage from "./client-page";
 
-interface ProtectedLayoutProps {
-  children: React.ReactNode;
-}
-
-export default async function ProtectedLayout({
-  children,
-}: ProtectedLayoutProps) {
+export default async function Page() {
   const supabase = await createClient();
   const user = await getUserWithDetailsQuery(supabase);
 
@@ -21,9 +15,9 @@ export default async function ProtectedLayout({
     return redirect("/onboarding");
   }
 
-  //   if (user.userDetails?.first_name && user.userDetails?.last_name) {
-  //     return redirect("/emails");
-  //   }
+  if (user.userDetails?.first_name && user.userDetails?.last_name) {
+    return redirect("/emails");
+  }
 
   return (
     <div className="flex h-screen w-full items-center justify-center bg-gradient-to-b from-card/100 to-background/60">
