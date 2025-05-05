452f70d664e665f2fdf739931337d74d1372902e
Thomas Harmond <teharmond@gmail.com>
2025-05-05 16:22:53 -0500

    wip

--- apps/web/components/marketing/sections/links.tsx
+++ apps/web/components/marketing/sections/links.tsx
@@ -1,6 +1,16 @@
+"use client";
+
 import React from "react";
+import Image from "next/image";
+import { useTheme } from "next-themes";
+import qrDarkSvg from "../svg/qr-dark.svg";
+import qrLightSvg from "../svg/qr-light.svg";
+import linkDarkSvg from "../svg/link-dark.svg";
+import linkLightSvg from "../svg/link-light.svg";
 
 export default function LinksSection() {
+  const { theme } = useTheme();
+
   return (
     <div className="grid w-full grid-cols-1 gap-4">
       <div className="rounded-xl border p-4">
@@ -9,22 +19,35 @@ export default function LinksSection() {
       <div className="rounded-xl border p-4">
         <h2 className="text-2xl font-bold">Automations</h2>
       </div>
-      <div className="flex w-full flex-col items-center gap-4">
-        <h2 className="text-3xl font-bold">Links and QR Codes</h2>
-        <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2">
-          <div className="flex w-full flex-col items-center justify-center gap-2">
-            {/* image here */}
+      <div className="flex w-full flex-col gap-16">
+        <div className="flex w-full items-center justify-center gap-12">
+          <Image
+            src={theme === "dark" ? qrLightSvg : qrDarkSvg}
+            alt="QR Code illustration"
+            width={100}
+            height={100}
+            className="w-full max-w-[300px]"
+          />
+          <div className="flex flex-col items-start gap-2">
             <h2 className="text-xl font-semibold">QR Codes</h2>
-            <p className="max-w-sm text-pretty text-center text-muted-foreground">
+            <p className="max-w-sm text-pretty text-left text-muted-foreground">
               Manage your QR codes like never before. Create multiple for the
               same link to see where they were scan, update where they link to
               at any time, and deisgn your codes to match your brand.{" "}
             </p>
           </div>
-          <div className="flex w-full flex-col items-center justify-center gap-2">
-            {/* image here */}
+        </div>
+        <div className="mt-16 flex w-full items-center justify-center gap-12">
+          <Image
+            src={theme === "dark" ? linkLightSvg : linkDarkSvg}
+            alt="Link page illustration"
+            width={96}
+            height={96}
+            className="w-full max-w-[300px]"
+          />
+          <div className="flex flex-col items-start gap-2">
             <h2 className="text-xl font-semibold">Link Pages</h2>
-            <p className="max-w-sm text-pretty text-center text-muted-foreground">
+            <p className="max-w-sm text-pretty text-left text-muted-foreground">
               Create pages for your important links and resources to share with
               your congregation. They&apos;re perfect for sharing links in your
               social media bios.
