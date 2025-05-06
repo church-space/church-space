392b258bcfaf7174604369d9cd551bef148d9723
Thomas Harmond <teharmond@gmail.com>
2025-05-06 16:01:25 -0500

    wip

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -4,7 +4,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@church-space/ui/toaster";
 import { TooltipProvider } from "@church-space/ui/tooltip";
-import Head from "next/head";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,6 +11,22 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Church Space",
   description: "Church Space",
+  openGraph: {
+    title: "Church Space",
+    description: "The better way to email your church",
+    url: "https://churchspace.co",
+    siteName: "Church Space",
+    images: [
+      {
+        url: "https://churchspace.co/og-image.jpg", // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+    ],
+
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
@@ -21,32 +36,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Head>
-        {/* <script src="https://unpkg.com/react-scan/dist/auto.global.js" /> */}
-        <meta
-          property="og:title"
-          content="The better way to email your church"
-        />
-        <meta
-          property="og:description"
-          content="Church Space is a platform that helps churches send emails to their members."
-        />
-        <meta
-          property="og:image"
-          content="https://churchspace.co/og-image.jpg"
-        />
-        <meta property="og:url" content="https://churchspace.co/" />
-        <meta property="og:type" content="website" />
-
-        {/* Twitter Card */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Church Space" />
-        <meta name="twitter:description" content="Church Space" />
-        <meta
-          name="twitter:image"
-          content="https://churchspace.co/og-image.jpg"
-        />
-      </Head>
+      {/* <head> */}
+      {/* <script src="https://unpkg.com/react-scan/dist/auto.global.js" /> */}
+      {/* </head> */}
       {/* <ReactScan /> */}
       <body className={`${inter.className} antialiased`}>
         <TooltipProvider delayDuration={300}>
