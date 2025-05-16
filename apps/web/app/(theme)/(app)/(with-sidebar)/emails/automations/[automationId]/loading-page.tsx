72f135cfb8c8782591b029ebf3ec324728bd8286
Thomas Harmond <teharmond@gmail.com>
2025-05-16 08:01:19 -0500

    wip

--- apps/web/app/(theme)/(app)/(with-sidebar)/emails/automations/[automationId]/loading-page.tsx
+++ apps/web/app/(theme)/(app)/(with-sidebar)/emails/automations/[automationId]/loading-page.tsx
@@ -1,6 +1,3 @@
-import React from "react";
-import { SidebarTrigger } from "@church-space/ui/sidebar";
-import { Separator } from "@church-space/ui/separator";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -8,12 +5,12 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@church-space/ui/breadcrumb";
-import Link from "next/link";
 import { Button } from "@church-space/ui/button";
+import { Separator } from "@church-space/ui/separator";
+import { SidebarTrigger } from "@church-space/ui/sidebar";
+import { Skeleton } from "@church-space/ui/skeleton";
 import { Ellipsis } from "lucide-react";
-import DataTableLoading from "@/components/tables/data-table-loading";
-import { columns } from "@/components/tables/automation-members/columns";
-import { Email, HourglassClock } from "@church-space/ui/icons";
+import Link from "next/link";
 
 export default function LoadingPage() {
   return (
@@ -24,18 +21,15 @@ export default function LoadingPage() {
           <Separator orientation="vertical" className="mr-2 h-4" />
           <Breadcrumb>
             <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <Link prefetch={true} href="/emails">
-                  Emails
-                </Link>
-              </BreadcrumbItem>
+              <Link href="/emails" className="hidden md:block">
+                <BreadcrumbItem>Emails</BreadcrumbItem>
+              </Link>
               <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem className="hidden md:block">
-                <Link prefetch={true} href="/emails/automations">
-                  Automations
-                </Link>
-              </BreadcrumbItem>
+              <Link href="/emails/automations" className="hidden md:block">
+                <BreadcrumbItem>Automations</BreadcrumbItem>
+              </Link>
               <BreadcrumbSeparator className="hidden md:block" />
+
               <BreadcrumbItem>
                 <BreadcrumbPage>Loading...</BreadcrumbPage>
               </BreadcrumbItem>
@@ -46,93 +40,16 @@ export default function LoadingPage() {
           <Button variant="ghost" size="icon">
             <Ellipsis className="h-4 w-4" />
           </Button>
-
-          <Button disabled className="cursor-pointer" variant="default">
-            Enabled
+          <Button disabled variant="default">
+            Enable
           </Button>
         </div>
       </header>
-
-      <div className="mx-auto w-full flex-1 px-4 py-10 md:px-12">
-        <div className="mx-auto flex max-w-6xl flex-col space-y-10">
-          {/* Link Information Section */}
-          <div className="flex w-full justify-between gap-4 border-b pb-4">
-            <div className="flex w-full items-center justify-between gap-4">
-              <div className="group flex-1">
-                <div className="flex items-center">
-                  <div className="flex items-center gap-2">
-                    <div className="flex flex-row items-center gap-2">
-                      <h2 className="text-2xl font-bold text-muted-foreground transition-colors group-hover:text-primary">
-                        Loading...
-                      </h2>
-                    </div>
-                  </div>
-                </div>
-                <p className="mt-1 text-muted-foreground">Loading...</p>
-              </div>
-            </div>
+      <div className="min-h-[calc(100vh-10rem)] w-full px-8 pt-3">
+        <div className="flex min-h-[calc(100vh-7rem)] w-full items-center justify-center px-5">
+          <div className="flex flex-col items-center gap-3">
+            <div className="text-muted-foreground">Getting things ready...</div>
           </div>
-          <div className="flex flex-col gap-4 rounded-lg border bg-gradient-to-r from-accent/80 to-accent p-4 shadow-sm md:p-6 md:pb-2 md:pt-4">
-            <div className="flex items-center justify-between">
-              <div className="text-lg font-bold">Steps</div>
-
-              <Button variant={"outline"} size={"sm"}>
-                Edit Trigger & Steps
-              </Button>
-            </div>
-            <div className="relative w-full">
-              <div className="flex flex-col gap-2 pb-4 sm:flex-row sm:overflow-x-auto sm:pe-16">
-                <div className="flex items-center gap-2">
-                  <div className="w-full sm:h-16 sm:w-28">
-                    <div className="flex h-full w-full flex-shrink-0 items-center gap-2 truncate rounded-md border border-muted-foreground bg-muted p-2 py-1.5 text-sm text-muted-foreground sm:flex-col sm:justify-between">
-                      <span className="flex-shrink-0">
-                        <Email height={"24"} width={"24"} />
-                      </span>
-                      <span className="truncate">Send Email</span>
-                    </div>
-                  </div>
-                  <div className="hidden text-xs text-muted-foreground sm:block">
-                    →
-                  </div>
-                  <div className="w-full sm:h-16 sm:w-28">
-                    <div className="flex h-full w-full flex-shrink-0 items-center gap-2 rounded-md border border-muted-foreground bg-muted p-2 py-1.5 text-sm text-muted-foreground sm:flex-col sm:justify-between">
-                      <span className="flex-shrink-0">
-                        <HourglassClock height={"24"} width={"24"} />
-                      </span>
-                      <span className="truncate">Wait 5 days</span>
-                    </div>
-                  </div>
-
-                  <div className="hidden text-xs text-muted-foreground sm:block">
-                    →
-                  </div>
-                  <div className="w-full sm:h-16 sm:w-28">
-                    <div className="flex h-full w-full flex-shrink-0 items-center gap-2 truncate rounded-md border border-muted-foreground bg-muted p-2 py-1.5 text-sm text-muted-foreground sm:flex-col sm:justify-between">
-                      <span className="flex-shrink-0">
-                        <Email height={"24"} width={"24"} />
-                      </span>
-                      <span className="truncate">Send Email</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="absolute right-0 top-0 hidden h-full w-20 bg-gradient-to-l from-accent via-accent/80 to-transparent sm:block" />
-            </div>
-          </div>
-          <>
-            <div className="flex w-full flex-col justify-between gap-3 md:flex-row md:items-center">
-              <div className="flex flex-col">
-                <h1 className="text-2xl font-bold">
-                  <span className="font-normal text-muted-foreground">0</span>{" "}
-                  Automation Members
-                </h1>
-                <p className="text-sm text-muted-foreground">
-                  Note: a person can only go through the automation once.
-                </p>
-              </div>
-            </div>
-            <DataTableLoading columns={columns} />
-          </>
         </div>
       </div>
     </div>
